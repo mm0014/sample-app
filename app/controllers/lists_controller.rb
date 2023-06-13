c160447f51257fcf2df19535a43bc0419230b4c1
mm0014 <132628218+mm0014@users.noreply.github.com>
2023-06-13 16:15:23 +0000

    Initial Commit1

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,52 @@
+class ListsController < ApplicationController
+  def new
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = List.new
+  end
+
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    @list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    if @list.save
+    flash[:notice] = "投稿が成功しました"
+    # 詳細画面へリダイレクト
+    redirect_to list_path(@list.id)
+  else
+    render :new
+  end
+end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    # 送られてくるデータはparamに入っている
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+    def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+    def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
+
+  end
+
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
+end
